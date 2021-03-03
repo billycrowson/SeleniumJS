5883062e60492b872b3e21cf1a03300042a6e4c6
billycrowson <billy.crowson@cgi.com>
2021-03-02 21:46:19 -0600

    add delays to watch test

--- test/specs/testExample.js
+++ test/specs/testExample.js
@@ -4,7 +4,9 @@ describe("Lambdatest Demo Test", function() {
     it("Lambdatest Demo TestCase", function() {
         browser.url("https://lambdatest.github.io/sample-todo-app/");
         $("*[name='li1']").click();
+        browser.pause(1000);
         $("*[name='li2']").click();
+        browser.pause(1000);
        $("#sampletodotext").setValue("Lambdatest\n");
         
         assert.strictEqual(browser.getTitle(), "Sample page - lambdatest.com");
